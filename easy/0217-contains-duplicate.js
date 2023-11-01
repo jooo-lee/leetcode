5f9e703dc6f0b27904f32a050b56143ff8f023c0
Joseph Lee <josephlee3004@gmail.com>
2023-11-01 18:40:38 -0400

    Finish 0217-contains-duplicate

---
+++ easy/0217-contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    const set = new Set();
+
+    for (num of nums) {
+        // We can exit early if duplicate is found
+        if (set.has(num)) return true;
+        set.add(num);
+    }
+
+    return false;
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
