15a3f2267f6349708df36187c62962f54449dce1
Joseph Lee <josephlee3004@gmail.com>
2023-11-07 11:18:48 -0500

    Add two pointer solution to 0125-valid-palindrome

--- easy/0125-valid-palindrome.js
+++ easy/0125-valid-palindrome.js
@@ -3,12 +3,45 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    if (s.length == 1) return true;
-    // Remove all non-alphanumeric chars (including spaces)
+    // Remove non-alphanumeric characters - O(n)
     let sMod = s.replace(/[^a-zA-Z0-9]/g, "");
-    // Make all uppercase letters lowercase
+
+    // Convert any uppercase letters into lowercase letters - O(n)
     sMod = sMod.toLowerCase();
-    // Reverse and compare with non-reversed modified string
-    sModRev = sMod.split("").reverse().join("");
-    return sMod == sModRev;
+
+    let lPtr = 0;
+    let rPtr = sMod.length - 1;
+
+    // Compare character at lPtr with character at rPtr - O(n)
+    while (lPtr < rPtr) {
+        // Characters not matching - not valid palindrome
+        if (sMod[lPtr] != sMod[rPtr]) return false;
+
+        // Move pointers so we can compare next two characters
+        lPtr++;
+        rPtr--;
+    }
+
+    // Valid palindrome found
+    return true;
 };
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// where n is the length of s
+
+// ---------------------- Alternate solution: ----------------------
+// var isPalindrome = function (s) {
+//     if (s.length == 1) return true;
+//     // Remove all non-alphanumeric chars (including spaces)
+//     let sMod = s.replace(/[^a-zA-Z0-9]/g, "");
+//     // Make all uppercase letters lowercase
+//     sMod = sMod.toLowerCase();
+//     // Reverse and compare with non-reversed modified string
+//     sModRev = sMod.split("").reverse().join("");
+//     return sMod == sModRev;
+// };
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// where n is the length of s
