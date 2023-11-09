349c0e80b209d79d5e7c5b426b615cc2bd9f0c53
Joseph Lee <josephlee3004@gmail.com>
2023-11-09 11:08:18 -0500

    Modify solution to 0020-valid-parentheses

--- easy/0020-valid-parentheses.js
+++ easy/0020-valid-parentheses.js
@@ -3,26 +3,44 @@
  * @return {boolean}
  */
 var isValid = function (s) {
+    // Use an array as a stack
     const stack = [];
-    for (let i = 0; i < s.length; i++) {
-        switch (s[i]) {
-            case "(":
-                stack.push(")");
-                break;
-            case "{":
-                stack.push("}");
-                break;
-            case "[":
-                stack.push("]");
-                break;
-            case ")":
-            case "}":
-            case "]":
-                if (stack.pop() != s[i]) return false;
-                break;
-            default:
-                break;
+
+    // Create a hashmap with closing parentheses as keys and corresponding
+    // opening parentheses as values
+    const map = new Map();
+    map.set(")", "(");
+    map.set("}", "{");
+    map.set("]", "[");
+
+    // O(n)
+    for (const paren of s) {
+        /* 
+        If the current parenthesis is closing, first check if the stack is empty.
+        If so, return false, as we know that we have a closing parenthesis without
+        a corresponding opening parenthesis. 
+        If the stack is not empty, pop from the stack and check that the parenthesis
+        popped matches the current closing parenthesis (if they do not match, return
+        false).
+
+        If the current parenthesis is opening, we can simply push it to the stack.
+        */
+
+        if (map.has(paren)) {
+            // Closing parenthesis case
+            if (stack.length == 0 || stack.pop() != map.get(paren)) {
+                return false;
+            }
+        } else {
+            // Opening parenthesis case
+            stack.push(paren);
         }
     }
+
+    // Make sure all opening parentheses have corresponding closing parentheses
     return stack.length == 0;
 };
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// where n is the length of s
