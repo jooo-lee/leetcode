789de578e12e4a36ec8f3adfecb6a84e98967652
Joseph Lee <josephlee3004@gmail.com>
2023-11-13 15:12:56 -0500

    Update 0704-binary-search

--- easy/0704-binary-search.js
+++ easy/0704-binary-search.js
@@ -4,32 +4,36 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    // This will be O(log n) time complexity because we are
-    // splitting the search area in half each time we compare target
+    // This will be O(log n) time complexity because we are splitting
+    // the search area in half each time we go through one loop iteration
 
     // Keep track of bounds of search area
-    let left = 0;
-    let right = nums.length - 1;
+    let start = 0;
+    let end = nums.length - 1;
 
-    // If left > right then we know that there are no more
+    // If start > end then we know that there are no more
     // elements to be searched and we have not found the target
-    while (left <= right) {
-        let mid = Math.round((left + right) / 2);
-        // Look at the middle element
-        if (target == nums[mid]) {
-            // We have found the target element
-            return mid;
-        } else if (target < nums[mid]) {
+    while (start <= end) {
+        let middle = Math.round((start + end) / 2);
+
+        if (target < nums[middle]) {
             // Target is smaller than the middle element
             // So we search to the left of the middle element
-            right = mid - 1;
-        } else if (target > nums[mid]) {
+            end = middle - 1;
+        } else if (target > nums[middle]) {
             // Target is larger than the middle element
             // So we search to the right of the middle element
-            left = mid + 1;
+            start = middle + 1;
+        } else {
+            // We have found the target element
+            return middle;
         }
     }
 
     // Target not found
     return -1;
 };
+
+// Time complexity: O(log n)
+// Space complexty: O(1)
+// where n is the size of the nums array
