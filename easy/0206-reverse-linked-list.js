a99ab5c09d5a6ff243667585f57a1964bb6d6ff5
Joseph Lee <josephlee3004@gmail.com>
2023-11-20 11:40:29 -0500

    Add recursive solution to 0206-reverse-linked-list

--- easy/0206-reverse-linked-list.js
+++ easy/0206-reverse-linked-list.js
@@ -17,10 +17,8 @@ var reverseList = function (head) {
 
     // Stop loop when curr points to null - O(n)
     while (curr) {
-        /* 
-        Store curr.next so that we can break the link between curr 
-        and curr.next without worrying about losing the rest of the LL
-        */
+        // Store curr.next so that we can break the link between curr
+        // and curr.next without worrying about losing the rest of the LL
         next = curr.next;
 
         // Reverse curr.next by setting it to point to prev
@@ -35,8 +33,36 @@ var reverseList = function (head) {
     return prev;
 };
 
-/* 
-Time complexity: O(n)
-Space complexity: O(1)
-where n is the length of our LL
-*/
+// Time complexity: O(n)
+// Space complexity: O(1)
+// where n is the length of our LL
+
+// ------------------------- Alternate solution -------------------------
+// var reverseList = function (head) {
+//     // Recursive solution
+
+//     // Base case - if we have an empty LL or we have reached the
+//     // end of the LL (in both cases we have a reversed LL - empty
+//     // LL is reversed and LL of one ListNode is reversed)
+//     if (!head) return head;
+
+//     // Keep track of head of reversed LL
+//     let newHead = head;
+//     // We have not reached the end of the LL, keep going - O(n)
+//     if (head.next) {
+//         // Make recursive call, reverse the rest of the LL
+//         newHead = reverseList(head.next);
+
+//         // Reverse link (append current ListNode to reversed LL)
+//         head.next.next = head;
+
+//         // Set current ListNode (current tail of reversed LL) to point to null
+//         head.next = null;
+//     }
+
+//     return newHead;
+// };
+
+// Time complexity: O(n)
+// Space complexity: O(n) - we are making n recursive calls (n calls in our call stack)
+// where n is the length of our LL
