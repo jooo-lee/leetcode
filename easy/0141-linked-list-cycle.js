d9e3c15d096b763274015aa95839ef75f40bec69
Joseph Lee <josephlee3004@gmail.com>
2023-11-30 18:41:39 -0500

    Add slow and fast pointers solution to 0141-linked-list-cycle

--- easy/0141-linked-list-cycle.js
+++ easy/0141-linked-list-cycle.js
@@ -28,3 +28,38 @@ var hasCycle = function (head) {
 // Time complexity: O(n)
 // Space complexity: O(n)
 // where n is the length of the LL
+
+// ------------------------- Alternate solution -------------------------
+
+var hasCycle = function (head) {
+    // Edge cases - no cycle
+    if (!head || !head.next) return false;
+
+    // Initialize slow and fast pointers
+    let slow = head;
+    let fast = head.next;
+
+    /* 
+    Fast will reach the end of the LL before slow (if there is no cycle), 
+    so if it points to null, then we know that there is no cycle. We also need
+    to check whether fast.next is null or not at each iteration of the loop, since
+    we are moving fast by two nodes. 
+
+    If there is no cycle, slow and fast will never meet since fast moves twice as
+    fast as slow. However, if there is a cycle, they are bound to meet. NeetCode's 
+    video explanation of why they are bound to meet is pretty good.
+    */
+    while (fast && fast.next) {
+        // Slow and fast pointers have met each other - cycle found!
+        if (slow == fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // No cycle found
+    return false;
+};
+
+// Time complexty: O(n)
+// Space complexity: O(1)
+// where n is the length of the LL
