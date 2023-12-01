ccccae1561f8ba731acab66f63c6c9afacc31f9d
Joseph Lee <josephlee3004@gmail.com>
2023-12-01 11:34:56 -0500

    Update 0226-invert-binary-tree

--- easy/0226-invert-binary-tree.js
+++ easy/0226-invert-binary-tree.js
@@ -15,9 +15,7 @@ var invertTree = function (root) {
     if (root == null) return null;
 
     // Swap children of current root node
-    let tmp = root.left;
-    root.left = root.right;
-    root.right = tmp;
+    [root.left, root.right] = [root.right, root.left];
 
     // Recursive calls to continue swapping children
     invertTree(root.left);
@@ -25,3 +23,8 @@ var invertTree = function (root) {
 
     return root;
 };
+
+// Time complexity: O(n) - we are visiting each node
+// n is the total number of nodes in our binary tree
+// Space complexity: O(h) - we have at most h calls in our recursive call stack at a time
+// h is the height of our binary tree
