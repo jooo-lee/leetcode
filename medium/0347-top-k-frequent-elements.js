d69601e7ad9206d3e05bf6377437a1807fcad56e
Joseph Lee <josephlee3004@gmail.com>
2024-11-09 16:02:45 -0500

    Finish 0347-top-k-frequent-elements

---
+++ medium/0347-top-k-frequent-elements.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  // Count occurences using a hashmap
+  const occurences = new Map();
+  nums.forEach((num) => {
+    if (occurences.has(num)) {
+      occurences.set(num, occurences.get(num) + 1);
+    } else {
+      occurences.set(num, 1);
+    }
+  });
+
+  // Sort hashmap in ascending order of values/occurences
+  const sortedMap = [...occurences].sort((a, b) => b[1] - a[1]);
+
+  const res = [];
+  for (let i = 0; i < k; i++) {
+    res.push(sortedMap[i][0]);
+  }
+  return res;
+
+  // One liner of above block of code
+  // return sortedMap.slice(0, k).map(pair => pair[0]);
+};
+
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
