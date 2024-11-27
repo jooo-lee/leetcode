91a4ace3d24f3704c382be4d620d6eb7fff13e2b
Joseph Lee <josephlee3004@gmail.com>
2024-11-27 17:53:43 -0500

    Add optimal solution to 0125

--- easy/0125-valid-palindrome.js
+++ easy/0125-valid-palindrome.js
@@ -3,27 +3,62 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    // Remove non-alphanumeric characters - O(n)
-    let sMod = s.replace(/[^a-zA-Z0-9]/g, "");
+  let l = 0,
+    r = s.length - 1;
 
-    // Convert any uppercase letters into lowercase letters - O(n)
-    sMod = sMod.toLowerCase();
-
-    let lPtr = 0;
-    let rPtr = sMod.length - 1;
-
-    // Compare character at lPtr with character at rPtr - O(n)
-    while (lPtr < rPtr) {
-        // Characters not matching - not valid palindrome
-        if (sMod[lPtr] != sMod[rPtr]) return false;
-
-        // Move pointers so we can compare next two characters
-        lPtr++;
-        rPtr--;
+  while (l < r) {
+    while (l < r && !alphaNum(s[l])) {
+      l++;
     }
 
-    // Valid palindrome found
-    return true;
+    while (l < r && !alphaNum(s[r])) {
+      r--;
+    }
+
+    if (s[l].toLowerCase() != s[r].toLowerCase()) {
+      return false;
+    }
+
+    l++, r--;
+  }
+
+  return true;
+};
+
+const alphaNum = (c) => {
+  return (
+    (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+  );
+};
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// where n is the length of s
+
+// ---------------------- Alternate solution: ----------------------
+
+var isPalindrome = function (s) {
+  // Remove non-alphanumeric characters - O(n)
+  let sMod = s.replace(/[^a-zA-Z0-9]/g, '');
+
+  // Convert any uppercase letters into lowercase letters - O(n)
+  sMod = sMod.toLowerCase();
+
+  let lPtr = 0;
+  let rPtr = sMod.length - 1;
+
+  // Compare character at lPtr with character at rPtr - O(n)
+  while (lPtr < rPtr) {
+    // Characters not matching - not valid palindrome
+    if (sMod[lPtr] != sMod[rPtr]) return false;
+
+    // Move pointers so we can compare next two characters
+    lPtr++;
+    rPtr--;
+  }
+
+  // Valid palindrome found
+  return true;
 };
 
 // Time complexity: O(n)
@@ -33,18 +68,18 @@ var isPalindrome = function (s) {
 // ---------------------- Alternate solution: ----------------------
 
 var isPalindrome = function (s) {
-    if (s.length == 1) return true;
+  if (s.length == 1) return true;
 
-    // Remove all non-alphanumeric chars (including spaces)
-    let sMod = s.replace(/[^a-zA-Z0-9]/g, "");
+  // Remove all non-alphanumeric chars (including spaces)
+  let sMod = s.replace(/[^a-zA-Z0-9]/g, '');
 
-    // Make all uppercase letters lowercase
-    sMod = sMod.toLowerCase();
+  // Make all uppercase letters lowercase
+  sMod = sMod.toLowerCase();
 
-    // Reverse and compare with non-reversed modified string
-    sModRev = sMod.split("").reverse().join("");
+  // Reverse and compare with non-reversed modified string
+  sModRev = sMod.split('').reverse().join('');
 
-    return sMod == sModRev;
+  return sMod == sModRev;
 };
 
 // Time complexity: O(n)
