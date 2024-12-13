970b160408d8fadba3abef2b28d1b4acec8d8d05
Joseph Lee <josephlee3004@gmail.com>
2024-12-13 16:51:26 -0500

    Add O(n) memory solution to 0042

---
+++ hard/0042-trapping-rain-water.js
@@ -0,0 +1,38 @@
+// -------------------------- O(n) memory solution --------------------------
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let maxWater = 0;
+  const maxLeft = [];
+  const maxRight = [];
+
+  // Populate prefix array
+  for (let i = 0; i < height.length; i++) {
+    if (i == 0) {
+      maxLeft.push(0);
+      continue;
+    }
+    maxLeft.push(Math.max(maxLeft[maxLeft.length - 1], height[i - 1]));
+  }
+
+  // Populate suffix array
+  for (let i = height.length - 1; i >= 0; i--) {
+    if (i == height.length - 1) {
+      maxRight.push(0);
+      continue;
+    }
+    maxRight.push(Math.max(maxRight[maxRight.length - 1], height[i + 1]));
+  }
+  maxRight.reverse();
+
+  for (let i = 0; i < height.length; i++) {
+    maxWater += Math.max(0, Math.min(maxLeft[i], maxRight[i]) - height[i]);
+  }
+
+  return maxWater;
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
