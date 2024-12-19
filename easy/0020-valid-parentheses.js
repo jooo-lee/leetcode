1ab904f0ed4162d19adb6b9d354b32410a9a7e33
Joseph Lee <josephlee3004@gmail.com>
2024-12-18 20:25:03 -0500

    Add alternative solution to 0020

--- easy/0020-valid-parentheses.js
+++ easy/0020-valid-parentheses.js
@@ -3,19 +3,57 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    // Use an array as a stack
-    const stack = [];
+  const stack = [];
 
-    // Create a hashmap with closing parentheses as keys and corresponding
-    // opening parentheses as values
-    const map = new Map();
-    map.set(")", "(");
-    map.set("}", "{");
-    map.set("]", "[");
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case '(':
+        stack.push(')');
+        break;
+      case '{':
+        stack.push('}');
+        break;
+      case '[':
+        stack.push(']');
+        break;
+      case ')':
+      case '}':
+      case ']':
+        if (!stack.length || stack.pop() != s[i]) {
+          return false;
+        }
+        break;
+      default:
+        throw new Error('Non bracket input!');
+    }
+  }
 
-    // O(n)
-    for (const paren of s) {
-        /* 
+  return stack.length == 0;
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+// -------------------------- Hashmap solution --------------------------
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  // Use an array as a stack
+  const stack = [];
+
+  // Create a hashmap with closing parentheses as keys and corresponding
+  // opening parentheses as values
+  const map = new Map();
+  map.set(')', '(');
+  map.set('}', '{');
+  map.set(']', '[');
+
+  // O(n)
+  for (const paren of s) {
+    /* 
         If the current parenthesis is closing, first check if the stack is empty.
         If so, return false, as we know that we have a closing parenthesis without
         a corresponding opening parenthesis. 
@@ -26,21 +64,20 @@ var isValid = function (s) {
         If the current parenthesis is opening, we can simply push it to the stack.
         */
 
-        if (map.has(paren)) {
-            // Closing parenthesis case
-            if (stack.length == 0 || stack.pop() != map.get(paren)) {
-                return false;
-            }
-        } else {
-            // Opening parenthesis case
-            stack.push(paren);
-        }
+    if (map.has(paren)) {
+      // Closing parenthesis case
+      if (stack.length == 0 || stack.pop() != map.get(paren)) {
+        return false;
+      }
+    } else {
+      // Opening parenthesis case
+      stack.push(paren);
     }
+  }
 
-    // Make sure all opening parentheses have corresponding closing parentheses
-    return stack.length == 0;
+  // Make sure all opening parentheses have corresponding closing parentheses
+  return stack.length == 0;
 };
 
 // Time complexity: O(n)
 // Space complexity: O(n)
-// where n is the length of s
